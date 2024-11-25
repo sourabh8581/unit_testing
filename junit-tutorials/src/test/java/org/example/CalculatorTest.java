7bbb4fcec3a145e1b1a02fa72eb4099b4b4d4a05
sourabh8581 <sourabhmali2017@gmail.com>
2024-11-25 19:55:37 +0530

    Test naming

--- junit-tutorials/src/test/java/org/example/CalculatorTest.java
+++ junit-tutorials/src/test/java/org/example/CalculatorTest.java
@@ -2,16 +2,30 @@ package org.example;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Calculator Tests")
+//@Disabled
 class CalculatorTest {
 
+  @DisplayName("Addition Test")
   @Test
   public void addTest(){
     Calculator calculator = new Calculator();
     int actualResult = calculator.add(10, 20);
 
     assertEquals(30,actualResult);
+  }
+  @DisplayName("Subtract Test")
+  @Test
+  @Disabled("Disabled untill feature is completely developed")
+  public void subtractTest(){
+    Calculator calculator = new Calculator();
+    int actualResult = calculator.subtract(20, 10);
+
+    assertEquals(10,actualResult);
   }
 
 }
